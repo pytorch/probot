1029969010227b7729a07f6e4ef334489226c9ba
Edward Z. Yang <ezyang@fb.com>
2019-10-30 11:54:01 -0400

    Modularize the bot
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,11 @@
 const nock = require('nock')
 const myProbotApp = require('..')
 const { Probot } = require('probot')
+const { nockTracker } = require('./common.js')
 
 nock.disableNetConnect()
 
-describe('My Probot app', () => {
+describe('index (integration test for all actions)', () => {
   let probot
 
   beforeEach(() => {
@@ -13,23 +14,7 @@ describe('My Probot app', () => {
     app.app = () => 'test'
   })
 
-  function nockTracker (contents) {
-    const configPayload = require('./fixtures/config.json')
-    configPayload['content'] = Buffer.from(`
-tracking_issue: 6
-`).toString('base64')
-    nock('https://api.github.com')
-      .get('/repos/ezyang/testing-ideal-computing-machine/contents/.github/pytorch-probot.yml')
-      .reply(200, configPayload)
-
-    const payload = require('./fixtures/issue.json')
-    payload['body'] = contents
-    nock('https://api.github.com')
-      .get('/repos/ezyang/testing-ideal-computing-machine/issues/6')
-      .reply(200, payload)
-  }
-
-  test('add a cc when issue is labeled', async () => {
+  test('when issue is labeled', async () => {
     nock('https://api.github.com')
       .post('/app/installations/1492531/access_tokens')
       .reply(200, { token: 'test' })
@@ -56,69 +41,4 @@ Some header text
 
     scope.done()
   })
-
-  test('update an existing cc when issue is labeled', async () => {
-    nock('https://api.github.com')
-      .post('/app/installations/1492531/access_tokens')
-      .reply(200, { token: 'test' })
-
-    nockTracker(`
-Some header text
-
-* testlabel @ezyang
-`)
-
-    const payload = require('./fixtures/issues.labeled')
-    payload['issue']['body'] = 'Arf arf\n\ncc @moo @foo/bar @mar\nxxxx'
-
-    const scope = nock('https://api.github.com')
-      .patch('/repos/ezyang/testing-ideal-computing-machine/issues/5', (body) => {
-        expect(body).toMatchObject({
-          body: 'Arf arf\n\ncc @ezyang @moo @foo/bar @mar\nxxxx'
-        })
-        return true
-      })
-      .reply(200)
-
-    await probot.receive({ name: 'issues', payload })
-
-    scope.done()
-  })
-
-  test('mkldnn update bug', async () => {
-    nock('https://api.github.com')
-      .post('/app/installations/1492531/access_tokens')
-      .reply(200, { token: 'test' })
-
-    nockTracker(`* module: mkldnn @gujinghui @PenghuiCheng @XiaobingSuper @ezyang`)
-
-    const payload = require('./fixtures/issues.labeled')
-    payload['issue']['body'] = `its from master branch, seems related with mklml. any idea?
-
-cc @ezyang`
-    payload['issue']['labels'] = [
-      { name: 'module: mkldnn' }
-    ]
-
-    const scope = nock('https://api.github.com')
-      .patch('/repos/ezyang/testing-ideal-computing-machine/issues/5', (body) => {
-        expect(body).toMatchObject({
-          body: `its from master branch, seems related with mklml. any idea?
-
-cc @gujinghui @PenghuiCheng @XiaobingSuper @ezyang`
-        })
-        return true
-      })
-      .reply(200)
-
-    await probot.receive({ name: 'issues', payload })
-
-    scope.done()
-  })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
