fb5155c60ef4f93d2aebb93f1dffd79aac0afcf1
Edward Z. Yang <ezyang@fb.com>
2019-10-30 12:24:18 -0400

    Add auto-label bot
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ auto-label-bot.js
@@ -0,0 +1,41 @@
+module.exports = app => {
+  app.on('issues.labeled', async context => {
+    // Careful!  For most labels, we only apply actions *when the issue
+    // is added*; not if the issue is pre-existing (for example, high
+    // priority label results in triage review, but if we unlabel it
+    // from triage review, we shouldn't readd triage review the next
+    // time the issue is labeled).
+
+    const label = context.payload["label"]["name"]
+    const labels = context.payload['issue']['labels'].map(e => e['name'])
+    context.log({ label, labels })
+
+    const labelSet = new Set(labels);
+
+    newLabels = []
+    function addLabel(l) {
+      if (!labelSet.has(l)) {
+        newLabels.push(l);
+        labelSet.add(l);
+      }
+    }
+
+    // NB: Added labels here will trigger more issues.labeled actions,
+    // so be careful about accidentally adding a cycle.  With just label
+    // addition it's not possible to infinite loop as you will
+    // eventually quiesce, beware if you remove labels though!
+    switch (label) {
+      case 'high priority':
+        addLabel('triage review')
+        break;
+      case 'topic: flaky-tests':
+        addLabel('high priority')
+        addLabel('triage review')
+        break;
+    }
+
+    if (newLabels.length) {
+      await context.github.issues.addLabels(context.issue({ labels: newLabels }))
+    }
+  })
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const autoCcBot = require('./auto-cc-bot.js')
+const autoLabelBot = require('./auto-label-bot.js')
 
 module.exports = app => {
   autoCcBot(app)
+  autoLabelBot(app)
 }
