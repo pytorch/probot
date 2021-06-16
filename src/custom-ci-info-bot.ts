1ee98d3314641d2eb8306cc3461369b2238c0177
Sam Estep <sestep@fb.com>
2021-06-16 11:39:37 -0700

    Fix lint

--- src/custom-ci-info-bot.ts
+++ src/custom-ci-info-bot.ts
@@ -9,15 +9,21 @@ _Hello,_ **world!**
 </details>
 `.trim();
 
+// this has other fields too but we don't use them
+interface Label {
+  name: string;
+}
+
 export default function myBot(app: probot.Application): void {
   app.on('pull_request.edited', async context => {
     const {owner, repo} = context.repo();
     const {number, labels, body} = context.payload.pull_request;
     context.log.info({owner, repo, number, body}, 'custom CI info start');
-    if (!labels.map(label => label.name).includes('ci/future')) {
+    if (!labels.map((label: Label) => label.name).includes('ci/future')) {
       context.log.info('ignoring PR because it has no ci/future label');
       return;
     }
+    // eslint-disable-next-line @typescript-eslint/camelcase
     const update = {owner, repo, pull_number: number, body: dummy};
     context.log.info(update, 'updating PR body');
     await context.github.pulls.update(update);
