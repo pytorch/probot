43ab17139bbf38393af53687b788f9d5ed30ee65
Eli Uriegas <eliuriegas@fb.com>
2021-07-08 12:04:28 -0700

    Add traceback translation for typescript
    Exceptions were hard to read through the transpiled javascript so this
    adds the source-map-support module which should allow for tracebacks to
    be translated back to their equivalent typescript lines.
    
    See https://www.npmjs.com/package/source-map-support
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import {install} from 'source-map-support';
+
 import autoCcBot from './auto-cc-bot';
 import autoLabelBot from './auto-label-bot';
 import triggerCircleCiBot from './trigger-circleci-workflows';
 import {Application} from 'probot';
+
+// Needed for traceback translation from transpiled javascript -> typescript
+install();
 
 function runBot(app: Application): void {
   autoCcBot(app);
