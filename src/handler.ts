9b4f74109950b2382d1c169f260f6b7179f05f6f
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2021-09-01 12:19:33 -0700

    Add transpiled traceback translation for handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,8 @@
 import {serverless} from '@probot/serverless-lambda';
+import {install} from 'source-map-support';
 import appFn from './';
+
+// Needed for traceback translation from transpiled javascript -> typescript
+install();
+
 module.exports.probot = serverless(appFn);
