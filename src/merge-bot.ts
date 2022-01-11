a6109d33b251f2819a4f4b86b6e31a26a938de81
Nikita Shulga <nshulga@fb.com>
2022-01-11 09:04:39 -0800

    Replaced comment with reaction for merge bot

--- src/merge-bot.ts
+++ src/merge-bot.ts
@@ -6,6 +6,7 @@ function mergeBot(app: probot.Application): void {
     const commentBody = ctx.payload?.comment?.body;
     const owner = ctx.payload?.repository?.owner?.login;
     const repo = ctx.payload?.repository?.name;
+    const commentId = ctx.payload?.comment?.id;
     const prNum = ctx.payload?.issue?.number;
     if (commentBody?.match(cmdPat)) {
       await ctx.github.repos.createDispatchEvent({
@@ -16,11 +17,11 @@ function mergeBot(app: probot.Application): void {
           pr_num: prNum
         }
       });
-      await ctx.github.issues.createComment({
-        body: 'Hello World',
+      await ctx.github.reactions.createForIssueComment({
+        comment_id: commentId,
+        content: '+1',
         owner,
-        repo,
-        issue_number: prNum
+        repo
       });
     }
   });
